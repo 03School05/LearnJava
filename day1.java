081d3997d6b51871a9573dff4e65a14c7a42a91e
School <truongcongnguyen1@gmail.com>
2024-11-05 23:46:14 +0700

    toi da them 1 thu muc vao

--- day1.java
+++ day1.java
@@ -1,7 +1,6 @@
-public class day1
- {
+public class day1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //mới tập git
+        // mới tập git
     }
 }
